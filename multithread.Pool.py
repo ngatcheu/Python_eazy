0005a5a97cef85fe34183f776e884a42b128f8ed
ngatcheu <40948971+ngatcheu@users.noreply.github.com>
2022-06-28 11:12:02 +0200

    Create multithread.Pool.py

---
+++ multithread.Pool.py
@@ -0,0 +1,22 @@
+#Nouvelle classe python pour le multithreading: ThreadPoolExecutor
+
+from concurrent.futures import ThreadPoolExecutor
+
+values = [3,4,5,6,7,8]
+
+def cube(x):
+    print(f'Cube of {x}:{x*x*x}')
+
+if __name__ == '__main__':
+
+    result = []
+    #Definir le nombre de worker 
+    with ThreadPoolExecutor(max_workers=6) as exe:
+        exe.submit(cube,2)
+
+        #Maps the method 'cube' with the list of values.
+        result = exe.map(cube,values)
+
+
+    for r in result:
+            print(r)
